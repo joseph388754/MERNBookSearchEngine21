8658cacf7faf72c99c0811950928e65c7f24a057
unknown <joseph_malonzo100@yahoo.com>
2023-09-04 19:43:55 -0700

    server side - server.js apollo server implemented

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,6 +18,22 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
+const stApolloServer = async (typeDefs, resolvers) => {
+  await Server.start();
+  Server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+    console.log(`Use GraphQL at http://localhost:${PORT}${Server.graphqlPath}`);
+  })
+}
+
+stApolloServer(typeDefs, resolvers);
+
+
