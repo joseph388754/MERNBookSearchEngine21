7cb13356cd9739c60376dabe6e21e61560b42f26
unknown <joseph_malonzo100@yahoo.com>
2023-09-05 07:35:50 -0700

    created apollo provider on app.js finished

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,42 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+//import apolloprovider
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <Router>
-      <>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
         <Navbar />
-        <Routes>
+          <Routes>
           <Route 
             path='/' 
             element={<SearchBooks />} 
@@ -23,8 +53,9 @@ function App() {
             element={<h1 className='display-2'>Wrong page!</h1>}
           />
         </Routes>
-      </>
-    </Router>
+        </>
+     </Router>
+    </ApolloProvider>
   );
 }
 
